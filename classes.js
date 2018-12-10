7c69823f0bc99662154d168c98df0346743b4f2e
DazLamas <sarah.d.lamas@gmail.com>
2018-12-10 13:13:13 +0100

    Refactor. Gym - Centers - Members - Workers

--- classes.js
+++ classes.js
@@ -1,58 +1,82 @@
-// 1 - Vamos a elaborar un sistema telemático para gestionar una red de Gimnasios, lo iremos ampliando poco a poco en futuros ejercicios.
-//
-// Lo primero que debes hacer es crear las siguientes clases:
-//
-// Gimnasio - nombre, CIF, centros (conjunto de gimnasios distribuidos por el país).
-// Centro - dirección, localidad, código postal, gerente (Persona), socios (Socio). // añadirsocio eleminarsocio
-
-// Persona - nombre, apellido, edad, sexo, NIF.
-// Socio - Similar a la persona pero además contendrá númeroSocio.
-// El número de socio se debe asignar automáticamente cada vez que se añade un socio nuevo al Centro.
-
-
-class Gym {//abstracta!!!!
-  constructor(name, cif) {
-    this.cif     = cif;
-    this.name    = name;
+//ToDo:
+// - prohibir instanciar clase
+// - añadir métodos: listCenters, listWorkers, listMembers
+class Gym {
+  constructor() {
+    // if (new.target === Gym) {
+    //  throw new TypeError("Cannot construct Abstract instances directly.");
+    // }
+    this.cif     = "";
+    this.name    = "";
     this.centers = new Map([]);
+    this.workers = new Map([]);
   }
 
-  addCenter(center) {
-    this.centers.set(center.nif, center);
+  createAndAddCenter(cif, address, city, zip, manager) {
+    this.centers.set(cif, new Center(cif, address, city, zip, manager));
   }
 
   removeCenter(centerNif) {
     this.centers.delete(center.nif);
   }
 
+  createAndAddWorker(nif, name, sirname, age, gender, role, availableHours, monthlySalary, center) {
+
+    const newWorker = new Worker(nif, name, sirname, age, gender, role, availableHours, monthlySalary, center);
+
+    this.workers.set(nif, newWorker);
+
+    if (center) {
+      center.workers.set(newWorker.nif, newWorker);
+    };
+  }
+
+  removeWorker(workerNif) {
+    this.workers.delete(worker.nif);
+  }
+
 }
 
 class Center {
-  constructor(cif, address, city, zip, manager = 'no registered yet') {
+  constructor(cif, address, city, zip, manager = 'not registered yet') {
     this.cif     = cif;
     this.address = address;
     this.city    = city;
     this.zip     = zip;
-    this.manager = manager;
+    this.workers = new Map([]);
     this.members = new Map([]);
+    // this.manager = manager;
+    // this.rooms   = new Set([]);
+    // this.classes = new Set([]);
   };
 
-  setManager(name, sirname, age, gender, nif) {
-    this.manager = new Worker(name, sirname, age, gender, nif);
-  };
+  // setManager(name, sirname, age, gender, nif) {
+  //   this.manager = new Manager(name, sirname, age, gender, nif);
+  // };
 
-  addMember(name, sirname, age, gender, nif, memberRef) {
-    this.members.set(nif, new Member(name, sirname, age, gender, nif, memberRef));
+  createAndAddMember(nif, name, sirname, age, gender, memberRef, contractedRates) {
+    this.members.set(nif, new Member(nif, name, sirname, age, gender, memberRef, contractedRates));
   };
 
   removeMember(nif) {
     this.members.delete(nif);
   };
 
+  // addRoom(room) {
+  //   this.rooms.add(room);
+  // }
+  //
+  // removeRoom(room) {
+  //   this.rooms.delete(room.type);
+  // }
+
 };
 
 class Person {
   constructor(nif, name, sirname, age, gender) {
+    if (new.target === Person) {
+     throw new TypeError("This is an Abstract class.");
+    }
     this.nif     = nif;
     this.name    = name;
     this.sirname = sirname;
@@ -61,36 +85,72 @@ class Person {
   };
 };
 
-class Room {
-  constructor(type, size, hasWindows, equipment) {
-    this.type       = type;
-    this.size       = size;
-    this.hasWindows = hasWindows;
-    this.equipment  = ['esterillas', ...equipment];
-  };
-   getTools() {
-    return this.equipment;
+class Member extends Person {
+  constructor(nif, name, sirname, age, gender, contractedRates) {
+    super(nif, name, sirname, age, gender)
+    this.memberRef       = nif;
+    this.contractedRates = {
+      type: contractedRates.type,
+      accessingHours: contractedRates.accessingHours
+    };
   };
 };
 
 class Worker extends Person {
-  constructor(nif, name, sirname, age, gender, role){ //Necesario para declarar las propiedades de una clase
-    super(nif, name, sirname, age, gender) //Llama a las propiedades de la clase padre
-    this.role = role;
-  }
-};
-
-class Member extends Person {
-  constructor(nif, name, sirname, age, gender, memberRef) {
+  constructor(nif, name, sirname, age, gender, role, availableHours, monthlySalary, center) {
     super(nif, name, sirname, age, gender)
-    this.memberRef = memberRef;
+    this.role  = role;
+    this.availableHours = availableHours;
+    this.monthlySalary  = monthlySalary;
+    this.center         = center;
   };
 };
+//
+// class Room {
+//   constructor(type, size, hasWindows, equipment) {
+//     this.type       = type;
+//     this.size       = size;
+//     this.hasWindows = hasWindows;
+//     this.equipment  = ['esterillas', ...equipment];
+//   };
+//    listEquipment() {
+//     return this.equipment;
+//   };
+// };
+//
+// class Timetable {
+//   constructor(open, close, classFrecuency) {
+//     this.open           = open;
+//     this.close          = close;
+//     this.timetable      = new Map([]);
+//   };
+//   createTimetable(classInfo) {
+//     this.timetable.set(classInfo.hour, classInfo.subject)
+//   };
+//   listTimetable() {
+//     return this.timetable;
+//   };
+//   hasClass(subject) {
+//     // return Object.keys(this.timetable).find(key => this.timetable[key] === subject);
+//   };
+// };
+//
+// class Class {
+//   constructor(hour, subject, room, teacher) {
+//     this.hour    = hour;
+//     this.subject = subject;
+//     this.room    = room;
+//     this.teacher = teacher;
+//   };
+// };
+//
 
-class PilatesRoom extends Room {
-
-};
-
-class CoreRoom extends Room {
-
-};
+//
+// class Teacher extends Worker {
+//   constructor(nif, name, sirname, age, gender, hours, habilities) {
+//     super(nif, name, sirname, age, gender)
+//     this.hours = hours;
+//     this.habilities = habilities;
+//   };
+// };
+//
--- workflow.js
+++ workflow.js
@@ -1,30 +1,56 @@
 
 
-const alta_fit     = new Gym("Alta Fit", 324529475);
-const af_moratalaz = new Center(32842763, "Camino de los Vinateros", "Madrid", 28029);
-
-alta_fit.addCenter(af_moratalaz);
-af_moratalaz.setManager('Catalina', 'Muñoz Esteban', 43, 'mujer', 3243582);
-
-af_moratalaz.addMember('Cristina', 'Ferraz Escribano', 37, 'mujer', 4053585);
-af_moratalaz.addMember('Raúl', 'Ferraz Escribano', 30, 'hombre', 4883583);
-af_moratalaz.addMember('María', 'Ferraz Escribano', 26, 'mujer', 4053344);
-af_moratalaz.removeMember(4883583);
-
-const sala_pilates_morata = new PilatesRoom(10, true, ["balón medicinal", "espalderas"]);
-const sala_maquinas_morata = new CoreRoom(10, true, ["máquina abdominales", "maquina sentadillas"]);
+const altaFit = new Gym();
+altaFit.cif  = '324529I';
+altaFit.name = 'Alta Fit';
+// const timetable = new Timetable(9, 21, 1);
 
 
-
-console.log(alta_fit);
-console.log(af_moratalaz);
-
-// console.log(sala_pilates_morata.getTools());
-// console.log(sala_maquinas_morata.getTools());
+// * Creating and adding new center:
+altaFit.createAndAddCenter('324525H', 'Camino de los Vinateros', 'Madrid', '28029');
 
 
-const manager = new Worker(3243582, 'Catalina', 'Muñoz Esteban', 43, 'mujer', 'manager');
+// * Create new member and assign to its center
+const afMoratlaz = altaFit.centers.get("324525H");
+afMoratlaz.createAndAddMember('4883583F', 'Raúl', 'Ferraz Escribano', 30, 'hombre', {type: 'full', accessingHours: [9,21]})
 
-const room1 = new PilatesRoom('pilates', 20, true, ['balón medicinal'])
-console.log(manager);
-console.log(person1);
+//  * Add new worker to its Gym and Center at the same time:
+altaFit.createAndAddWorker('232232P', 'Nico', 'Gauss Feet', 28, 'male', 'recepcionista', [9, 18], 1000, afMoratlaz)
+
+// * Creating worker before knowing the center
+altaFit.createAndAddWorker('139992C', 'Liz', 'Gauss Feet', 25, 'female', 'manager', [9, 18], 1800);
+
+//After a few days, you know the center in which this worker if going to work:
+const workerToAdd = altaFit.workers.get('139992C');
+const centerForTheNewWorker = altaFit.centers.get("324525H");
+
+centerForTheNewWorker.workers.set(workerToAdd.nif, workerToAdd);
+
+
+// afMoratlaz.setManager('Catalina', 'Muñoz Esteban', 43, 'mujer', '3243582C');
+
+
+// Members:
+// afMoratlaz.createAndAddMember('4883583F', 'Raúl', 'Ferraz Escribano', 30, 'hombre');
+// afMoratlaz.createAndAddMember('4053344D', 'María', 'Ferraz Escribano', 26, 'no-binario');
+// afMoratlaz.removeMember(4883583);
+
+
+//Rooms:
+// const salaPilates = new Room('pilates', 10, true, ["balón medicinal", "espalderas"]);
+// const salaCore    = new Room('core', 10, true, ["máquina abdominales", "maquina sentadillas"]);
+//
+// const class1 = new Class(9, 'pilates', 'nicola');
+// const class2 = new Class(10, 'yoga', 'carmen');
+// const class3 = new Class(12, 'gap', 'gonzalo');
+// const class4 = new Class(14, 'zumba', 'gabriel');
+//
+// timetable.createTimetable(class1);
+// timetable.createTimetable(class2);
+// timetable.createTimetable(class3);
+// timetable.createTimetable(class4);
+//
+// console.log(timetable.listTimetable());
+//
+console.log(altaFit);
+console.log(afMoratlaz);
